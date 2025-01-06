216ca89494afe2472ad061db1ec3f9c9dce09b71
Arawak <venomara2442@gamil.com>
2025-01-06 21:08:35 +0200

    Make calendar display by weeks

--- src/main/java/in/corpore/team/todaytomorrow/HelloController.java
+++ src/main/java/in/corpore/team/todaytomorrow/HelloController.java
@@ -1,14 +1,96 @@
 package in.corpore.team.todaytomorrow;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button monday;
+    @FXML
+    private Button tuesday;
+    @FXML
+    private Button wednesday;
+    @FXML
+    private Button thursday;
+    @FXML
+    private Button friday;
+    @FXML
+    private Button saturday;
+    @FXML
+    private Button sunday;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        monday.setText("Monday");
+        monday.setOnAction(event ->{
+            tuesday.setStyle("");
+            wednesday.setStyle("");
+            thursday.setStyle("");
+            friday.setStyle("");
+            saturday.setStyle("");
+            sunday.setStyle("");
+            monday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        });
+        tuesday.setOnAction(event -> {
+            monday.setStyle("");
+            wednesday.setStyle("");
+            thursday.setStyle("");
+            friday.setStyle("");
+            saturday.setStyle("");
+            sunday.setStyle("");
+            tuesday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+
+
+        });
+        wednesday.setOnAction(actionEvent -> {
+            monday.setStyle("");
+            tuesday.setStyle("");
+            thursday.setStyle("");
+            friday.setStyle("");
+            saturday.setStyle("");
+            sunday.setStyle("");
+            wednesday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        });
+        thursday.setOnAction(actionEvent -> {
+            monday.setStyle("");
+            tuesday.setStyle("");
+            wednesday.setStyle("");
+            friday.setStyle("");
+            saturday.setStyle("");
+            sunday.setStyle("");
+            thursday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        });
+        friday.setOnAction(actionEvent -> {
+            monday.setStyle("");
+            tuesday.setStyle("");
+            wednesday.setStyle("");
+            thursday.setStyle("");
+            saturday.setStyle("");
+            sunday.setStyle("");
+            friday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        });
+        saturday.setOnAction(actionEvent -> {
+            monday.setStyle("");
+            tuesday.setStyle("");
+            wednesday.setStyle("");
+            thursday.setStyle("");
+            friday.setStyle("");
+            sunday.setStyle("");
+            saturday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        });
+        sunday.setOnAction(actionEvent -> {
+            monday.setStyle("");
+            tuesday.setStyle("");
+            wednesday.setStyle("");
+            thursday.setStyle("");
+            friday.setStyle("");
+            saturday.setStyle("");
+            sunday.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        });
     }
 }
