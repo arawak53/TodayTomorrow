90d2a1eeb6cf5a07c152f86555aae01b83826e36
Arawak <venomara2442@gamil.com>
2025-01-06 14:52:24 +0200

    Create project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module in.corpore.team.todaytomorrow {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens in.corpore.team.todaytomorrow to javafx.fxml;
+    exports in.corpore.team.todaytomorrow;
+}
